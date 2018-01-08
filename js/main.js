05faba9358a56486c81e596ab2cbd30f43155c4b
estheryjeon <32020849+estheryjeon@users.noreply.github.com>
2018-01-07 23:13:12 -0600

    add banana and coffee

--- js/main.js
+++ js/main.js
@@ -3,10 +3,15 @@ $(document).ready(function() {
     $('.gif a img').hover(
       function() {
         var gif = $(this).parent().attr('data-gif');
-        console.log(gif);
         $(this).attr('src', gif);
       }, function() {
         $(this).attr('src', $(this).attr('data-orig'));
+      });
+
+    $('#times a').click(
+      function() {
+        var clicked = $(this).attr('id');
+        $('#current').attr('src', '../assets/coffee/' + clicked + '.jpg');
       });
 
 });
