ab80b4149f4441fd25f18f6337157c16fc0cd22d
Esther Jeon <estherjeon@Esthers-MacBook-Pro.local>
2019-03-31 21:46:29 -0400

    rejection and tokyo

--- js/main.js
+++ js/main.js
@@ -1,30 +1,4 @@
 $(document).ready(function() {
 
-/*
-  function hoverActive(link, des) {
-    if ($(link).hasClass('active')) {
-      $(des).css('visibility', 'visible');
-    } else {
-      $(link).hover(function() {
-        $(des).css('visibility','visible');
-        }, function() {
-        $(des).css('visibility','hidden');
-      })
-    }
-  }
-
-  hoverActive('#work','#made');*/
-
-
-  if ($('#smile').hasClass('active')) {
-    $('#smile').css('opacity', '1');
-  } else {
-    $('#smile').css('opacity', '0.5');
-    $('#smile').hover(function() {
-      $(this).css('opacity','1');
-      }, function() {
-      $(this).css('opacity','0.5');
-    })}
-
 
 });
