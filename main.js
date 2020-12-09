6ff98f99bdbb64a4fe1c14555c8eecd73346530c
Esther Jeon <estheryjeon@gmail.com>
2020-12-08 21:10:42 -0600

    some copy updates and cleaning up files

--- main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-
-  $( "#show" ).click(function() {
-    $("html").toggleClass("reflection-style");
-    $(".info").toggle();
-    $(".reflection").toggle();
-  });
-
-});
